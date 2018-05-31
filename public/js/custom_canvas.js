c06da5ff6e553a5e21add270efd77af62ae8c21a
ammyrai <rooprai.aman@gmail.com>
2018-05-31 13:34:16 +0530

    changes in the canvas script file and a new json file for color codes

--- public/js/custom_canvas.js
+++ public/js/custom_canvas.js
@@ -5,9 +5,16 @@ var gridSize = 25,                      // Grid Tile Size
     box,                                // Variable for rectangle element
     isMouseDown = false;                // Set Mouse down property false
 
-    // set fill pattern image
-    var imageObj = new Image();
-    imageObj.src = '../imgs/stich.png';
+
+    /* set fill pattern cross image for grid tiles starts here */
+    var bgImageObj = new Image();
+    bgImageObj.src = '../imgs/bgimg.png';
+    /* set fill pattern cross image for grid tiles ends here */
+
+    /* set fill pattern cross image for grid tiles starts here */
+    var stichImageObj = new Image();
+    stichImageObj.src = '../imgs/stich.png';
+    /* set fill pattern cross image for grid tiles ends here */
 
 /*  create stage for main canvas  */
 var stage = new Konva.Stage({
@@ -16,18 +23,25 @@ var stage = new Konva.Stage({
     height: canvasHeight * gridSize       // Canvas Height
 });
 
-/*    Create a background Layer for canvas   */
-var background = new Konva.Layer();
+/*  Create Multiple Layers for stage  */
 
-var text = new Konva.Text({
-  x: 30,
-  y: 30,
-  text: 'X',
-  fontSize: gridSize,
-  fontFamily: 'Calibri',
-  fill: 'red'
-});
-/*  Create a grid on canvas Layer */
+var backgroundCanvas = new Konva.Layer();        // Layer1 for canvas main background
+var canvasGridLayer = new Konva.Layer();         //  a Layer2 for canvas Grid
+
+/*  Layers creation ends here! */
+
+/*  Layer1 work starts here! */
+var stageRect =  new Konva.Rect({
+  x:0,
+  y:0,
+  width: canvasWidth * gridSize,
+  height: canvasHeight * gridSize,
+  fill: 'white',
+})
+backgroundCanvas.add(stageRect);
+/*  Layer1 work ends here! */
+
+/*  Layer2 Create a grid on canvas work starts here!*/
 for (var ix = 0; ix < canvasWidth; ix++) {
     for (var iy = 0; iy < canvasHeight; iy++) {
       box = new Konva.Rect({
@@ -35,43 +49,62 @@ for (var ix = 0; ix < canvasWidth; ix++) {
           y : iy * gridSize,
           width : gridSize ,
           height : gridSize,
-          stroke: '#ccc',
-          strokeWidth: 1,
-          lineJoin : 'round'
+          //strokeEnabled: false,
+          //dash: [4, 21],
+          stroke: '#FFE793',
+          strokeWidth: 0,
+          lineJoin : 'round',
+          shadowEnabled : true,
+          //fillPatternImage: stichImageObj,
+          shadowColor: '#FFE9AD',
+          shadowOffset: {  x: 3,   y: 3 },
+          shadowOpacity: 1
       });
-      background.add(box);              // Add rectangle to background layer
-    //  background.add(text);
+      //console.log(box);
+      var circle = new Konva.Circle({
+        x: box.attrs.x,
+        y: box.attrs.y,
+        radius: 2,
+        stroke: '#F7976F',
+        strokeWidth: 1,
+        fill: '#FED376',
+      });
+
+      canvasGridLayer.add(box);              // Add rectangle to background layer
+      canvasGridLayer.add(circle);              // Add rectangle to background layer
     }
   }
 
 /*    Fill Grid cell   */
-background.on('mousedown', function(evt) {
+canvasGridLayer.on('mousedown', function(evt) {
   isMouseDown = true
   if (isMouseDown){
     var box = evt.target;
     if(box.attrs.fillPatternImage) { }
     else {
-    box.fillPatternImage(imageObj);
+    box.fillPatternImage(stichImageObj);
     box.draw();
     }
   }
 });
-background.on('mouseup',function(evt){
+canvasGridLayer.on('mouseup',function(evt){
   isMouseDown= false
 })
 
-background.on('mouseover', function(evt) {
+canvasGridLayer.on('mouseover', function(evt) {
   if (isMouseDown){
     var box = evt.target;
     if(box.attrs.fillPatternImage){ }
     else {
-    box.fillPatternImage(imageObj);
+    box.fillPatternImage(stichImageObj);
     box.draw();
     }
   }
 });
+/*  Layer2 Create a grid on canvas work ends here!*/
 
-stage.add(background);          // Add Layer to stage
+stage.add(backgroundCanvas,canvasGridLayer);          // Add Layer to stage
+
 
 var json = stage.toJSON();      // Save entire canvas as json
 //console.log(json);
