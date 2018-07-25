e053ffb4816bd9d57c0d82f6f63c8b534332ad4e
ammyrai <rooprai.aman@gmail.com>
2018-07-25 18:18:53 +0530

    fixed the edit page drop down content

--- resources/views/patternedit.blade.php
+++ resources/views/patternedit.blade.php
@@ -67,25 +67,15 @@
                   {!! Form::close() !!}
 
                       <select width="200" id="selectTxtColor" placeholder="Select Your Favorite Colour for Canvas" data-search="true" data-item="txtColorSelect">
-                        <option value="#fcfbf8" data-type="">Ecru</option>
-                        <option value="#ffffff" data-type="">White</option>
-                        <option value="#000000" data-type="">Black</option>
-                        <option value="#c72b3b" data-type="">Red</option>
-                        <option value="#fd5d35" data-type="">Orange</option>
-                        <option value="#ffe300" data-type="">Yellow</option>
-                        <option value="#7fb335" data-type="">Green</option>
-                        <option value="#6b9ebf" data-type="">Blue</option>
-                        <option value="#633666" data-type="">Violet</option>
-
-                        <option value="#fcfbf8" data-type="">Ecru</option>
-                        <option value="#ffffff" data-type="">B5200</option>
-                        <option value="#000000" data-type="">310</option>
-                        <option value="#c72b3b" data-type="">321</option>
-                        <option value="#fd5d35" data-type="">608</option>
-                        <option value="#ffe300" data-type="">973</option>
-                        <option value="#7fb335" data-type="">906</option>
-                        <option value="#6b9ebf" data-type="">826</option>
-                        <option value="#633666" data-type="">327</option>
+                        <option value="#fcfbf8" data-type="">Ecru - Ecru</option>
+                        <option value="#ffffff" data-type="">White - B5200</option>
+                        <option value="#000000" data-type="">Black - 310</option>
+                        <option value="#c72b3b" data-type="">Red - 321</option>
+                        <option value="#fd5d35" data-type="">Orange - 608</option>
+                        <option value="#ffe300" data-type="">Yellow - 973</option>
+                        <option value="#7fb335" data-type="">Green - 906</option>
+                        <option value="#6b9ebf" data-type="">Blue - 826</option>
+                        <option value="#633666" data-type="">Violet - 327</option>
                         </select>
                 </div>
         </div>
