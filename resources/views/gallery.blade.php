c1b9fab993df6b59b520713c34d1c712d1c69b50
ammyrai <rooprai.aman@gmail.com>
2018-08-13 12:15:16 +0530

    some fixes in text limit gallery and pattern page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -27,7 +27,7 @@
                     <img src="{{ $image->pattern_img }}" width="150px" height="150px"/>
                     <p class="pattern_heading">{{ $image->pattren_name }}</p>
                     @if( !empty($image->pattern_info))
-                    <p>{!! substr($image->pattern_info, 0, 15) !!}@if( strlen($image->pattern_info) >= 15)<span id="dots_{{$image->id}}">...</span><span id="more_{{$image->id}}" class="moretext">{!! substr($image->pattern_info, 15) !!}</span></p><a href="javascript:void(0);" class="redmore_link" onclick="myFunction({{$image->id}})" id="myBtn_{{$image->id}}">Read more</a>@endif
+                    <p>{!! substr($image->pattern_info, 0, 15) !!}@if( strlen($image->pattern_info) > 15)<span id="dots_{{$image->id}}">...</span><span id="more_{{$image->id}}" class="moretext">{!! substr($image->pattern_info, 15) !!}</span></p><a href="javascript:void(0);" class="redmore_link" onclick="myFunction({{$image->id}})" id="myBtn_{{$image->id}}">Read more</a>@endif
                     @endif
                   </div>
                   <div class="actions">
